bb7e70855bb78d9ba3301b97dacdc5d24e2dc7b5
Guvunsu <gavryk_fray_rojas@hotmail.com>
2024-12-17 12:44:36 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WindowsFormsApp1 {
+    partial class Form1 {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btn_close = new System.Windows.Forms.Button();
+            this.txt_usuario = new System.Windows.Forms.Label();
+            this.btn_Ok = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt_contraseña = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(488, 308);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(134, 57);
+            this.btn_close.TabIndex = 0;
+            this.btn_close.Text = "Cerrar";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.AutoSize = true;
+            this.txt_usuario.Location = new System.Drawing.Point(183, 79);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(43, 13);
+            this.txt_usuario.TabIndex = 1;
+            this.txt_usuario.Text = "Usuario";
+            this.txt_usuario.Click += new System.EventHandler(this.txt_Welcome_Click);
+            // 
+            // btn_Ok
+            // 
+            this.btn_Ok.Location = new System.Drawing.Point(101, 308);
+            this.btn_Ok.Name = "btn_Ok";
+            this.btn_Ok.Size = new System.Drawing.Size(134, 57);
+            this.btn_Ok.TabIndex = 2;
+            this.btn_Ok.Text = "Ok";
+            this.btn_Ok.UseVisualStyleBackColor = true;
+            this.btn_Ok.Click += new System.EventHandler(this.btn_Ok_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(275, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt_contraseña
+            // 
+            this.txt_contraseña.AutoSize = true;
+            this.txt_contraseña.Location = new System.Drawing.Point(183, 144);
+            this.txt_contraseña.Name = "txt_contraseña";
+            this.txt_contraseña.Size = new System.Drawing.Size(61, 13);
+            this.txt_contraseña.TabIndex = 4;
+            this.txt_contraseña.Text = "Contraseña";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(275, 137);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(223, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txt_contraseña);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_Ok);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.btn_close);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Label txt_usuario;
+        private System.Windows.Forms.Button btn_Ok;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label txt_contraseña;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+        private void btn_Ok_Click(object sender, EventArgs e) {
+            //video numero 12 https://www.youtube.com/watch?v=CrDqrwlOvxk&list=PL8gxzfBmzgexdFa0XZZSZZn2Ogx3j-Qd5&index=12
+
+            //string textoDelTextbox = textBox1.Text; 
+            //txt_Welcome.Text = textoDelTextbox;
+
+            // video numero 13 https://www.youtube.com/watch?v=tg2MbXQQ_tc&list=PL8gxzfBmzgexdFa0XZZSZZn2Ogx3j-Qd5&index=14
+
+            //try {
+            //    byte textBox = Convert.ToByte(textBox1);
+            //    txt_Welcome.Text = textBox.ToString();
+            //} catch (OverflowException ex) {
+            //    MessageBox.Show("Error en el tamaño del numero " + ex);
+            //} catch (FormatException exx) {
+            //    MessageBox.Show("Error en el formato " + exx);
+            //}
+
+            // video numero 14 https://www.youtube.com/watch?v=tg2MbXQQ_tc&list=PL8gxzfBmzgexdFa0XZZSZZn2Ogx3j-Qd5&index=14
+
+            //try {
+            //    string bienvenido = "bienvenido al curso C#";
+            //    string texto = "hola" + textBox1.Text + bienvenido;
+            //    txt_Welcome.Text = texto;
+            //} catch (Exception x) {
+            //    MessageBox.Show("Error : " + x);
+            //}
+
+            //Modifique el Form para que dejara de tener el Welcome en el label y le puse usuario, para luego
+            // hacer el textbox2 y label2 usando ahora un texto lamado contraseña para que sirva el codigo de abajo
+            // video numero 15 https://www.youtube.com/watch?v=sqv3Rg0G-DQ&list=PL8gxzfBmzgexdFa0XZZSZZn2Ogx3j-Qd5&index=15
+
+            //    try {
+            //        if (txt_usuario.Text == "Eduardo" & txt_contraseña.Text == "1234") {
+            //            MessageBox.Show("Correcto,puedes pasar");
+            //        } else {
+            //            MessageBox.Show("Incorrecto,No puedes pasar");
+            //        }
+            //    } catch (Exception x) {
+            //        MessageBox.Show("Error" + 1);
+            //    }
+
+            //video numero 16 https://www.youtube.com/watch?v=P1_MMS_IKjg&list=PL8gxzfBmzgexdFa0XZZSZZn2Ogx3j-Qd5&index=16
+
+            string var_Usuario = txt_usuario.Text;
+            string var_Contraseña = txt_contraseña.Text;
+
+            try {
+                if (var_Usuario != "Eduardo" & Convert.ToInt32(var_Contraseña) + 50 == 100) {
+                    MessageBox.Show("Correcto,puedes pasar");
+                } else {
+                    MessageBox.Show("Incorrecto,No puedes pasar");
+                }
+            } catch (Exception x) {
+                MessageBox.Show("Error" + 1);
+            }
+
+        }
+    }
+}
+
